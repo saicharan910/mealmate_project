31a853af3bdf431ae98c340aee01f4da1e5da8f7
saicharan910 <thecharan122004@gmail.com>
2026-09-23 18:24:03 +0530

    Productionize build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --no-input
