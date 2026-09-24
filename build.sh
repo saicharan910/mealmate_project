60ef5f024f22051814d182d0500aef24592cdeee
saicharan910 <thecharan122004@gmail.com>
2026-09-24 10:12:03 +0530

    Refactor build script to simplify pip installation

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate --no-input
